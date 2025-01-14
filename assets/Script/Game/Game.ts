a24d418697cb5befb0c4d758520e77e528f20a95
hoangtronghanh23 <alextrong231@gmail.com>
2025-01-15 00:10:26 +0700

    add game manager + call event manager

--- assets/Script/Game/Game.ts
+++ assets/Script/Game/Game.ts
@@ -1,14 +1,28 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, director ,Component, Node, KeyCode } from 'cc';
 import { VirtualJoystic } from '../Input/VitualJoystic';
+import {EventManager} from "db://assets/Script/Game/Core/EventManager";
+import {ICanGetGameManager} from "db://assets/Script/Interface/ICanGetGameManager";
+import { CustomComponent } from './Core/CustomsEvent/CustomComponent';
+import { MultiInput } from '../Input/MultiInput';
+import { KeyboardInput } from '../Input/KeyBoardInput';
 const { ccclass, property } = _decorator;
 
 @ccclass('Game')
 export class Game extends Component {
-    @property(VirtualJoystic) private virtualJoystic: VirtualJoystic;
-
-    onLoad(){
-        this.virtualJoystic.init();
+    @property(VirtualJoystic) public virtualJoystic: VirtualJoystic;
+    eventManager: EventManager = null;
+    protected onLoad(){
+        this.eventManager = this.node.getComponent(EventManager);
+        director.getScene().getComponentsInChildren(CustomComponent).forEach((component) => {
+            (component as unknown as ICanGetGameManager).setGameManager(this);
+        })
+        this.SetUp();
     }
+    private SetUp(){
+        this.virtualJoystic.init();
+        this.eventManager.notifyObservers("SetUpPlayer");
+    }
+
 }
 
 
